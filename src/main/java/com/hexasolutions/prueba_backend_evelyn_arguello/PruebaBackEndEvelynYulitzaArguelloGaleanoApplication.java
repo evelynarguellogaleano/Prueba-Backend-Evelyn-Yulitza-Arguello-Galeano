d9ce83624e41f956c323f53153ac264b0bed84d3
bebelinn <145699248+bebelinn@users.noreply.github.com>
2025-12-24 08:07:27 -0500

    conexion con postgre

---
+++ src/main/java/com/hexasolutions/prueba_backend_evelyn_arguello/PruebaBackEndEvelynYulitzaArguelloGaleanoApplication.java
@@ -0,0 +1,13 @@
+package com.hexasolutions.prueba_backend_evelyn_arguello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackEndEvelynYulitzaArguelloGaleanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackEndEvelynYulitzaArguelloGaleanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexasolutions/prueba_backend_evelyn_arguello/PruebaBackEndEvelynYulitzaArguelloGaleanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexasolutions.prueba_backend_evelyn_arguello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackEndEvelynYulitzaArguelloGaleanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
